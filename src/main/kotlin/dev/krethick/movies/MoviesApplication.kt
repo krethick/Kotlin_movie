a69414cd6ddc07638ce62c825c4b3c70ce7c8db2
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-17 15:54:14 +0530

    Implemented the first api

--- src/main/kotlin/dev/krethick/movies/MoviesApplication.kt
+++ src/main/kotlin/dev/krethick/movies/MoviesApplication.kt
@@ -2,9 +2,17 @@ package dev.krethick.movies
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class MoviesApplication
+@RestController
+class MoviesApplication {
+	@GetMapping("/")
+	fun apiRoot(): String {
+		return "Hello, World"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<MoviesApplication>(*args)
