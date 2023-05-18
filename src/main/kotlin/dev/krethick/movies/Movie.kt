d7dda82cf2e8423a65a5a420f5e4df6037cc5f34
Krethick Chakravarthy <krethick.chakravarthy@gavait.com>
2023-05-18 11:54:37 +0530

    Working on the movie.kt file and included header files and annotated them

---
+++ src/main/kotlin/dev/krethick/movies/Movie.kt
@@ -0,0 +1,28 @@
+package dev.krethick.movies
+
+import lombok.AllArgsConstructor
+import lombok.Data
+import lombok.NoArgsConstructor
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+
+//We need to annotate as document
+@Document(collection = "movies")
+@Data    // comes from lombok header file (Use getter,setter and string methods)
+@AllArgsConstructor   // Takes all private field as argument
+@NoArgsConstructor // Takes no arguments/parameters
+class Movie {
+    //Also annotate the id field
+    @Id
+    // The ? after ObjectId indicates that the id variable can hold an ObjectId value or be null.
+    private var id: ObjectId? = null
+    private var imbId: String? = null
+    private var title: String? = null
+    private var releaseDate: String? = null
+    private var trailerLink: String? = null
+    private var poster: String? = null
+    private var genres: List<String>? = null
+    private var backdrops: List<String>? = null
+
+}
---
+++ src/main/kotlin/dev/krethick/movies/Review.kt
@@ -0,0 +1,4 @@
+package dev.krethick.movies
+
+class Review {
+}
